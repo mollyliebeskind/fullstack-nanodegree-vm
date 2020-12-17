7746e3a04e0fc98b774b59942d127b20f3ff2385
Molly Liebeskind <molly.liebeskind@gmail.com>
2020-12-17 16:33:49 -0500

    added flask-migration version control

--- class-demos/todoapp/app.py
+++ class-demos/todoapp/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import sys
 
 app = Flask(__name__)
@@ -7,11 +8,14 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://mollyliebeskind@localhost:54
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = "False"
 db = SQLAlchemy(app)
 
+migrate = Migrate(app, db)
+
 class ToDo(db.Model):
     # Define a todo class to create the to do table with columns id and description
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
+    # completed = db.Column(db.Boolean, nullable=False, default=False)
 
     # For debugging
     def __repr__(self):
@@ -47,45 +51,9 @@ def create_todo():
 def index():
     return render_template("index.html", data=ToDo.query.all())
 
-db.create_all()
+# comment this out if using migrate
+# db.create_all()
 
 
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=True)
-
-# MVC = model (database), views (what the client sees), controle (communication between views and models)
-# Get data from user through url addition, form, or json
-
-# from flask import Flask, render_template, request, redirect, url_for
-# from flask_sqlalchemy import SQLAlchemy
-
-# app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://mollyliebeskind@localhost:5432/todoapp'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'False'
-# db = SQLAlchemy(app)
-
-# class Todo(db.Model):
-#   __tablename__ = 'todos'
-#   id = db.Column(db.Integer, primary_key=True)
-#   description = db.Column(db.String(), nullable=False)
-
-#   def __repr__(self):
-#     return f'<Todo {self.id} {self.description}>'
-
-# db.create_all()
-
-# @app.route('/todos/create', methods=['POST'])
-# def create_todo():
-#   description = request.form.get('description', '')
-#   todo = Todo(description=description)
-#   db.session.add(todo)
-#   db.session.commit()
-#   return redirect(url_for('index'))
-
-# @app.route('/')
-# def index():
-#   return render_template('index.html', data=Todo.query.all())
-
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
